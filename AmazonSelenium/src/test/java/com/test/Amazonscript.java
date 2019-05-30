67d5c8391f2b70cc6b28cd199c0654c26cb45292
Divyamcm <mcmdivya@gmail.com>
2019-05-30 20:16:40 +0530

    third commit

--- AmazonSelenium/src/test/java/com/test/Amazonscript.java
+++ AmazonSelenium/src/test/java/com/test/Amazonscript.java
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Window;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -22,10 +23,10 @@ public class Amazonscript {
 	ArrayList<String> tabs;
 	String parentWindowHandle;
 
-	@Test
+	@Test(priority=0)
 	public void verifyCart() {
-		driver.findElement(By.xpath("//div[@id='nav-shop']//a")).click();
 		mainPageTitle = driver.getTitle();
+		driver.findElement(By.xpath("//div[@id='nav-shop']//a")).click();
 		parentWindowHandle = driver.getWindowHandle();
 		String selectLinkOpeninNewTab = Keys.chord(Keys.CONTROL, Keys.RETURN);
 		driver.findElement(By.xpath("//a[text()='Speakers']")).click();
@@ -34,8 +35,8 @@ public class Amazonscript {
 		WebElement product = driver.findElement(By.xpath(
 				"//li[@id='result_1']//a[@class='a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal']"));
 		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", product);
-		String price = driver.findElement(By.xpath("//li[@id='result_1']//span[@class='currencyINR']")).getText();
-		System.out.println(price);
+		String price = driver.findElement(By.xpath("//li[@id='result_1']//span[@class='a-size-base a-color-price s-price a-text-bold']")).getText();
+		System.out.println("Price Rs. "+price);
 		driver.findElement(By.xpath(
 				"//li[@id='result_1']//a[@class='a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal']"))
 				.sendKeys(selectLinkOpeninNewTab);
@@ -44,34 +45,27 @@ public class Amazonscript {
 		String productName = driver.findElement(By.xpath("//span[@id='productTitle']")).getText();
 		System.out.println(productName);
 		driver.findElement(By.xpath("//*[@title='Add to Shopping Cart']")).click();
-		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("xyz@gmail.com");
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("mcmdivya@gmail.com");
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
-		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("dbbmra");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("dbbmrctaa");
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
 		driver.findElement(By.xpath("//span[text()='Cart']")).click();
 		String cartName = driver.findElement(By.xpath(
 				"//div[@class='sc-list-body sc-java-remote-feature']//span[@class='a-size-medium sc-product-title a-text-bold']"))
 				.getText();
-		System.out.println("Cart" + cartName);
 		Assert.assertEquals(cartName, productName);
 		System.out.println("Product Name Matched");
 		driver.close();
+		driver.switchTo().window(parentWindowHandle);
 	}
 
-	@Test
+	@Test(priority=1)
 	public void VerifyHomePage() {
 
-		tabs = new ArrayList<String>(driver.getWindowHandles());
-		driver.switchTo().window(tabs.get(0));
 		WebElement logo = driver.findElement(By.xpath("//a[@aria-label='Amazon']"));
 		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", logo);
 		driver.findElement(By.xpath("//a[@aria-label='Amazon']")).click();
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		String titleAfterSwitching = driver.getTitle();
 		Assert.assertEquals(mainPageTitle, titleAfterSwitching);
 
@@ -90,7 +84,7 @@ public class Amazonscript {
 
 	@AfterSuite
 	public void afterClass() {
-		driver.close();
+		driver.quit();
 		System.exit(0);
 	}
 
